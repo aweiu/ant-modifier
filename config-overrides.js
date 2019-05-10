ede3c2bd4340388d3748f60d642b208c37a5cc11
aweiu <328080339@qq.com>
2019-05-10 11:56:48 +0800

    完善文档

--- config-overrides.js
+++ config-overrides.js
@@ -13,3 +13,30 @@ module.exports = override(
   // 开启 typescript lint
   enableEslintTypescript(),
 )
+
+// 下面注释掉的部分可以拿来启动一个完整项目
+// const {
+//   override,
+//   addDecoratorsLegacy,
+//   useEslintRc,
+//   addBabelPlugin,
+//   addLessLoader,
+//   enableEslintTypescript,
+// } = require('customize-cra')
+//
+// module.exports = override(
+//   // 开启 Decorators
+//   addDecoratorsLegacy(),
+//   // 使用自定义 eslintrc
+//   useEslintRc(),
+//   // 热更新
+//   addBabelPlugin('react-hot-loader/babel'),
+//   // Less
+//   addLessLoader({
+//     strictMath: true,
+//     noIeCompat: true,
+//     localIdentName: '[local]--[hash:base64:5]',
+//   }),
+//   // 开启 typescript lint
+//   enableEslintTypescript(),
+// )
