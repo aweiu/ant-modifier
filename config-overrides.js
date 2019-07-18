069c3e997c16441c852485e41763bf920c58610b
aweiu <328080339@qq.com>
2019-07-18 16:24:26 +0800

    升级构建配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,42 +1,22 @@
 const {
   override,
-  useEslintRc,
+  addDecoratorsLegacy,
   addBabelPlugin,
-  enableEslintTypescript,
+  // addLessLoader,
+  disableEsLint,
 } = require('customize-cra')
 
 module.exports = override(
-  // 使用自定义 eslintrc
-  useEslintRc(),
+  // 开启 Decorators
+  addDecoratorsLegacy(),
   // 热更新
   addBabelPlugin('react-hot-loader/babel'),
-  // 开启 typescript lint
-  enableEslintTypescript(),
+  // Less
+  // addLessLoader({
+  //   strictMath: true,
+  //   noIeCompat: true,
+  //   localIdentName: '[local]--[hash:base64:5]',
+  // }),
+  // 将 eslint 移入 commit 阶段
+  disableEsLint(),
 )
-
-// 下面注释掉的部分可以拿来启动一个完整项目
-// const {
-//   override,
-//   addDecoratorsLegacy,
-//   useEslintRc,
-//   addBabelPlugin,
-//   addLessLoader,
-//   enableEslintTypescript,
-// } = require('customize-cra')
-//
-// module.exports = override(
-//   // 开启 Decorators
-//   addDecoratorsLegacy(),
-//   // 使用自定义 eslintrc
-//   useEslintRc(),
-//   // 热更新
-//   addBabelPlugin('react-hot-loader/babel'),
-//   // Less
-//   addLessLoader({
-//     strictMath: true,
-//     noIeCompat: true,
-//     localIdentName: '[local]--[hash:base64:5]',
-//   }),
-//   // 开启 typescript lint
-//   enableEslintTypescript(),
-// )
